f901098aaf3b6f535c95057183d65afd692145fe
Nanjila David <nanjiladavid2@gmail.com>
2024-01-17 15:20:27 +0300

    Dealing with python strings

---
+++ string.py
@@ -0,0 +1,9 @@
+#Creating two names and strings
+name1 = "David"
+name2 = "Abisai"
+full_name = name1+ " " + name2
+full_name1 = f'{name1} {name2}'
+full_name2 = '{} {} {}'.format(name1, name2, full_name)
+print(full_name)
+print(full_name1)
+print(full_name2)
