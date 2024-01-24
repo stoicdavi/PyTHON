36e58acb28a88de557d3901846427d5824abf3a4
David Nanjila <nanjiladavid2@gmail.com>
2024-01-24 22:38:45 +0300

    created a file to use if else statement to determine the largest number

---
+++ check_a_greaterNo.py
@@ -0,0 +1,5 @@
+number1 = int(input("Enter the first Number: "))
+number2 = int(input("Enter the second Number: "))
+if number1 > number2: larger_no = number1
+else: larger_no = number2
+print("The largest number is:",larger_no)
