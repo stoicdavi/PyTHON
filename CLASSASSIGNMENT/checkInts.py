0ae0414c474696fcbcc6efe0e76fb20f0266cca6
David Nanjila <nanjiladavid2@gmail.com>
2024-02-01 16:29:28 +0300

    checking if the intergers are equal

---
+++ CLASSASSIGNMENT/checkInts.py
@@ -0,0 +1,7 @@
+#a code to check whether the entered integers are of the same value
+number1 = int(input("ENTER THE FIRST NUMBER "))
+number2 = int(input("ENTER THE SECOND NUMBER "))
+if number1 == number2:
+    print("The integers entered are of the same value")
+else: 
+    print("The integers entered are of different values")
---
+++ CLASSASSIGNMENT/heror.py
@@ -0,0 +1,5 @@
+#code to check generate an erroe if the negative value is entered
+number = int(input("Enter a number:"))
+if number < 0:
+    print("You have entered a negative number")
+    print("Please enter a positive number")
