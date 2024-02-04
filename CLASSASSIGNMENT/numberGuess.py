c27c1c07b49cf1748ac3ad2f1a94160663be06ee
David Nanjila <nanjiladavid2@gmail.com>
2024-02-04 23:44:59 +0300

    number guess source code

---
+++ CLASSASSIGNMENT/numberGuess.py
@@ -0,0 +1,13 @@
+#number guesing game
+import random
+print("Welcome to the number guessing game!")
+number = random.randint(20, 30)
+guess = int(input("Enter your guess: "))
+if guess == number:
+    print("Congratulations! You guessed the number!")
+elif guess < number:
+    print("Your guess is too low")
+elif guess > number:
+    print("Your guess is too high")
+else:
+    print("Sorry! The number was {}".format(number))
