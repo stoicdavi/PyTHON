3f7071970c2b46dc19fd786bd376a34a960c4b4e
David Nanjila <nanjiladavid2@gmail.com>
2024-02-09 11:21:20 +0300

    working with stings

---
+++ strings_Upper.py
@@ -0,0 +1,12 @@
+msg = "Welcome to Python david"
+print(msg.upper()) # WELCOME TO PYTHON
+print(msg.lower()) # welcome to python
+print(msg.capitalize()) # Welcome to python
+print(msg.title()) # Welcome To Python
+print(msg.swapcase()) # wELCOME TO pYTHON
+print(len(msg))
+print(msg.count('o')) 
+msg1 = "Welcome to Python 101: Strings"
+mani = msg1[-10]+" "+msg1[:8]+" "+msg1[25:30]+" "+msg1[8:10]+" Tyler"
+print(mani.title())
+print(mani[::-1].title())
