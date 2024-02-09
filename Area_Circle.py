3169c2b9b9bcdf85a28896f92a458c28126f0e26
David Nanjila <nanjiladavid2@gmail.com>
2024-02-09 10:42:09 +0300

    source codes to calculate area of a circle and reactagle

---
+++ Area_Circle.py
@@ -0,0 +1,6 @@
+#calculating area of a circle
+radius = int(input("Enter the radius"))
+def CirecleArea(radius):
+    result = 3.142 * radius**2
+    return result
+print(CirecleArea(2))
---
+++ rectangleArea.py
@@ -0,0 +1,8 @@
+#calculating the area of a rectangle
+length = int(input("Input the length: "))
+width = int(input("Enter width : "))
+
+def area1(length, width):
+    area2 = length * width
+    print("Your area is {}".format(area2))
+area1(length, width)
