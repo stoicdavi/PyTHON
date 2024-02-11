95304973d4c0abb64d2f96419d5362bac5244137
David Nanjila <nanjiladavid2@gmail.com>
2024-02-11 20:51:24 +0300

    added comments

--- CLASSASSIGNMENT/decisionInPython.py
+++ CLASSASSIGNMENT/decisionInPython.py
@@ -1,3 +1,7 @@
+# Write a program that calculates the total salary of an employee based on the following criteria:
+# If the employee has served for more than 10 years, he/she gets a bonus of 12% of the salary.
+# If the employee has served for 6 to 10 years, he/she gets a bonus of 10% of the salary.
+# If the employee has served for less than 6 years, he/she gets a bonus of 8% of the salary.
 salary = int(input("Enter your salary: "))
 yearsOfService = int(input("Enter your years of service: "))
 if yearsOfService > 10:
