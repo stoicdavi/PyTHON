b9a529b1f797e402c248da4369313a737faa4513
David Nanjila <nanjiladavid2@gmail.com>
2024-02-12 19:51:11 +0300

    added lines og code to check charge a bill

--- ElectricBill.py
+++ ElectricBill.py
@@ -1,10 +1,8 @@
-from typing import Union
 # Write a program to calculate the electricity bill. For the first 199 units, the charge is 1.20 per unit. For the next 200 units, the charge is 1.50 per unit. For the next 200 units, the charge is 1.80 per unit. For the next 200 units, the charge is 2.0 per unit. The program should accept the customer ID, customer name, and the number of units consumed and print the total charge using the customer name.
-CustomerId = int(input("Enter your customer ID :"))
-CustomerName = input("Enter your name :")
-UnitConsumed: Union[int, float] = float(input("enter the units consumed: "))
+from typing import Union
 
-def calculate(UnitConsumed: Union[int, float]) -> float:
+
+def calculate(UnitConsumed: Union[int, float]):
     if UnitConsumed <= 199:
         charge = UnitConsumed * 1.20
     elif UnitConsumed >= 200 and UnitConsumed <= 400:
@@ -14,5 +12,12 @@ def calculate(UnitConsumed: Union[int, float]) -> float:
     else:
         charge = UnitConsumed * 2.0
     return charge
-
-print("Dear {} your total charge is {}".format(CustomerName, calculate(UnitConsumed)))
+CustomerId = int(input("Enter your customer ID :"))
+CustomerName = input("Enter your name :")
+UnitConsumed: Union[int, float] = float(input("enter the units consumed: "))
+totalCharge = calculate(UnitConsumed)
+if totalCharge >= 400:
+    totalCharge1 = totalCharge * 0.15
+    print("Customer ID: {} \nUnits Consumed: {}\nCustomer Name: {}\nTotal charge: Ksh. {}".format(CustomerId,UnitConsumed,CustomerName, totalCharge + totalCharge1))
+else:
+    print("Dear {} your total charge is {}".format(CustomerName, totalCharge))
