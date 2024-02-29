cfdc6bd74c7227008eb60ec9147f7808d129631f
David Nanjila <nanjiladavid2@gmail.com>
2024-02-29 08:42:14 +0300

    data types in pyhon

---
+++ PRACTICLES/list_comprehension.py
@@ -0,0 +1,16 @@
+numbers = [number*5 for number in range(1, 6)]
+print(numbers)
+
+name = "John",
+name = "John",
+print(type(name))
+print(name[0])
+#dictionary
+capital_city = {"UK": "London", "USA": "Washington", "India": "New Delhi"}
+print(capital_city["UK"])
+capital_city["kenya"] = "Nairobi"
+print(capital_city)
+
+squares = {1:1, 2:4, 3:9, 4:16, 5:25}
+for i in squares:
+    print(squares[i])
