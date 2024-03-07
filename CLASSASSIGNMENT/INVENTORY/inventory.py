623eda96d35b17ab44f49c8122fddc43c83633cc
David Nanjila <nanjiladavid2@gmail.com>
2024-03-07 15:16:12 +0300

    create an while loop to iterate untill the user selects 4 to exit, if-elif-else statement to alow the user to input the type of product that he/she would like to add to the inventory

--- CLASSASSIGNMENT/INVENTORY/inventory.py
+++ CLASSASSIGNMENT/INVENTORY/inventory.py
@@ -10,28 +10,68 @@ class SimpleProduct(Product):
         self.unit_price = unit_price
     def calculate_value(self):
         return self.quantity_in_stock * self.unit_price
+    #a function to display the products details
+    def display(self):
+        return f"Product ID: {self.product_id}\nName: {self.name}\nQuantity in Stock: {self.quantity_in_stock}\nUnit Price: {self.unit_price}"
 class PerishableProduct(SimpleProduct):
     def __init__(self, product_id, name, quantity_in_stock, unit_price, expiry_date):
         super().__init__(product_id, name, quantity_in_stock, unit_price)
         self.expiry_date = expiry_date
     def calculate_value(self):
         pass
-
+    def display(self):
+        return f"Product ID: {self.product_id}\nName: {self.name}\nQuantity in Stock: {self.quantity_in_stock}\nUnit Price: {self.unit_price}\nExpiry Date: {self.expiry_date}"
 class DigitalProduct(Product):
     def __init__(self, product_id, name, quantity_in_stock, price):
         super().__init__(product_id, name, quantity_in_stock)
         self.price = price
     def calculate_value(self):
         pass
-product_id = input("Enter product_id: ")
-name = input("Enter product name")
-quantity_in_stock = int(input("Quantity in Stock"))
-unit_price = float(input("Enter the unit price"))
-expiryDate = input("Enter the return date (YYYY-MM-DD): ")
-expiry_date = datetime.strptime(expiryDate, "%Y-%m-%d")
-current_date = datetime.now()
-shelflife = expiry_date - current_date
+    def display(self):
+        return f"Product ID: {self.product_id}\nName: {self.name}\nQuantity in Stock: {self.quantity_in_stock}\nPrice: {self.price}"
 
-simple_product = SimpleProduct(name, product_id, quantity_in_stock, unit_price)
-print(simple_product.calculate_value())
-
+while True:
+    print("1. Simple Product")
+    print("2. Perishable Product")
+    print("3. Digital Product")
+    print("4. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        product_id = input("Enter product_id: ")
+        name = input("Enter product name: ")
+        quantity_in_stock = int(input("Quantity in Stock: "))
+        unit_price = float(input("Enter the unit price: "))
+        simple_product = SimpleProduct(name, product_id, quantity_in_stock, unit_price)
+        print("\nProduct Details")
+        simple_product.calculate_value()
+        print(simple_product.display())
+    elif choice == 2:
+        product_id = input("Enter product_id: ")
+        name = input("Enter product name: ")
+        quantity_in_stock = int(input("Quantity in Stock: "))
+        unit_price = float(input("Enter the unit price: "))
+        expiryDate = input("Enter the return date (YYYY-MM-DD): ")
+        expiry_date = datetime.strptime(expiryDate, "%Y-%m-%d")
+        perishable_product = PerishableProduct(name, product_id, quantity_in_stock, unit_price, expiry_date)
+        print("\nProduct Details")
+        perishable_product.calculate_value()
+        print(perishable_product.display())
+        file = open("inventory.txt", "a")
+        file.write(perishable_product.display())
+        file.close()
+    elif choice == 3:
+        product_id = input("Enter product_id: ")
+        name = input("Enter product name: ")
+        quantity_in_stock = int(input("Quantity in Stock: "))
+        price = float(input("Enter the price: "))
+        digital_product = DigitalProduct(name, product_id, quantity_in_stock, price)
+        print("\nProduct Details")
+        digital_product.calculate_value()
+        print(digital_product.display())
+        file = open("inventory.txt", "a")
+        file.write(digital_product.display())
+        file.close()
+    elif choice == 4:
+        break
+    else:
+        print("Invalid choice")
