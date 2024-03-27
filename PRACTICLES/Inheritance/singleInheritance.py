462bf1c68170d8f5c9d26702565ea4c2e3ea58d1
David Nanjila <nanjiladavid2@gmail.com>
2024-03-27 10:15:11 +0300

    chore:add  a code that implements inheritance and method overriding

--- PRACTICLES/Inheritance/singleInheritance.py
+++ PRACTICLES/Inheritance/singleInheritance.py
@@ -6,3 +6,15 @@ class Box:
         print("The box contains: {}".format(self.contents))
 my_box = Box("A pair of shoes")
 my_box.open_box()
+
+#single inheritance
+class GiftBox(Box):
+    def __init__(self, contents, recipient):
+        super().__init__(contents)
+        self.recipient = recipient
+    def open_box(self):#override the open_box method in super class
+        print(f"\nOpening the box.....")
+        print("The box contains: {}".format(self.contents))
+        print("The recipient is: {}".format(self.recipient))
+my_gift_box = GiftBox("A pair of shoes", "John")
+my_gift_box.open_box()
